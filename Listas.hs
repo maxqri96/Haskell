07af987d1f3b6c003668161fb6685e761e2c312d
Max <Max>
2017-01-11 21:37:35 -0200

    Testando Listas

--- Listas.hs
+++ Listas.hs
@@ -1,2 +1,13 @@
+-- Função que mostra o tamanho da lista 
+
 size_list [] = 0
-size_list (x:xs) = 1 + size_list xs -- Mostrando o tamanho da lista
+size_list (x:xs) = 1 + size_list xs
+
+-- Função que verifica se 2 listas são iguais 
+
+comp_listas :: [Int] -> [Int] -> Bool -- Passa 2 listas inteiras e retorna resultado bool
+comp_listas [] [] = True
+comp_listas [] _ = False
+comp_listas _ [] = False
+comp_listas (a:as) (b:bs) | (a == b) = comp_listas as bs
+						  | otherwise = False
